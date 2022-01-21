1eee473c58272300785241abebea02362752355c
yogeswar <yogi2198@gmail.com>
2022-01-21 01:57:42 -0500

    Time interval logic

--- main.py
+++ main.py
@@ -1,71 +1,48 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import pandas as pd
 import numpy as np
+
 #the size of dataset len(dataset)=3558412
 from datetime import datetime
-#dataset = open("DS/user-ct-test-collection-01.txt", "r").readlines()
 Q = pd.read_csv(f'DS/user-ct-test-collection-01.txt', sep='\t', header=0)
-print(Q.head(5))
-if Q['QueryTime'][0]!=Q['QueryTime'][1]:
-      print((Q['QueryTime'][2]), (Q['QueryTime'][3]))
+# print(Q.head(5))
+# if Q['QueryTime'][0]!=Q['QueryTime'][1]:
+#       print((Q['QueryTime'][2]), (Q['QueryTime'][3]))
 
 x= pd.Timedelta(abs(pd.Timestamp(Q['QueryTime'][2])- pd.Timestamp(Q['QueryTime'][3]))).seconds
-print(str(x))
+# print(str(x))
 x= 4
 y=6
 if pd.notnull(Q['ItemRank'][0]):
       print("yes")
 
-df = pd.DataFrame({'id': np.arange(1,6,1),
-                   'val': list('ABCDE')})
+# df = pd.DataFrame({'id': np.arange(1,6,1),
+#                    'val': list('ABCDE')})
 #print(Q['AnonID'])
-df = pd.DataFrame(columns=['AnonId', 'Query', 'Query change', 'Query Date', 'Query Time'])
-i=100
-iterator = 0
-list=[]
-while i <130:
-    #print("hiiii",i)
-    diff={}
-    diff["time"]=[]
-    l = Q['Query'][i]
-    ltime = Q['AnonID'][i]
-    user_name=[]
-    user_name.append(Q['Query'][i])
-    user_name.append(Q['QueryTime'][i])
-    #user_name[i]=[]
-    for j in range(i+1,130):
+# df = pd.DataFrame(columns=['index','AnonId', 'Query', 'Query change', 'Query Date', 'Query Time'])
+i=0
+group_config = Q.groupby('AnonID')
+for k,gp in group_config:
+    # if(k == 142):
+    filterWord = gp[gp["ClickURL"].notnull()]
+    processedData = filterWord["QueryTime"].astype('datetime64[ns]').to_frame().reset_index()
+    processedData.columns = ['index', 'QueryTime']
+    l=0
+    for l in processedData.index:
+        # config_var = gp.index[l] if k == 142 else gp.index[l-1]
+        config_var = gp.index[l]
+        prev_var_time =gp.loc[config_var]['QueryTime']
+        IntervalTimer = pd.Timedelta(abs(pd.Timestamp(processedData['QueryTime'][l]) - pd.Timestamp(prev_var_time))).seconds
+        if IntervalTimer >60:
+            print('('+str(k) + ','+gp.loc[filterWord.index[l]]['Query'] +',' +'NULL, [(1,' + gp.loc[filterWord.index[l]]['ClickURL']+')])')
+            # (142, westchester.gov, NULL, [(1, http: // www.westchestergov.com)])
+        else:
+            continue
 
-        if (Q['AnonID'][i]==Q['AnonID'][j]):
-              x = pd.Timedelta(abs(pd.Timestamp(Q['QueryTime'][i]) - pd.Timestamp(Q['QueryTime'][j]))).seconds
-              if x<=60:
-               # customDF = pd.DataFrame({'AnonID':Q['AnonID'][i],'Query':  Q['Query'][i],'Query change':Q['Query'][j],'Query Date':Q['QueryTime'][i], 'Query Time': Q['QueryTime'][j]},index=[0])
-                # newDF = pd.DataFrame({'AnonID':Q['AnonID'][i],'Query':  Q['Query'][i],'Query change':Q['Query'][j]}, index=[])
-                # df.to_csv(r'DS/final.txt', columns=["AnonID", "Query", "Query change"],index=False, sep=' ', mode='a')
-                l2=Q['Query'][j]+Q['QueryTime'][j]
-                if (Q['Query'][i] ==Q['Query'][j]):
-                   #user_name = []
-                   diff["time"].append(Q['QueryTime'][j])
-                   user_name.append(diff)
-                else:
-                    user_name.append(Q['Query'][j])
-                    diff["time"].append(Q['QueryTime'][j])
-                    user_name.append(diff)
-                    i = j
-               # df = df.append(customDF)
-                iterator += 1
-                #i = j
-              else:
-                  print(user_name)
-                  user_name=[]
-                  diff["time"]=[]
-                  # pass
-    if (len(user_name)!=0):
-        #customDF = pd.DataFrame(user_name)#({'AnonID': Q['AnonID'][i], 'Query': Q['Query'][i], 'Query change': Q['Query'][j], 'Query Date': Q['QueryTime'][i], 'Query Time': Q['QueryTime'][j]}, index=[0])
-        print("user_name")
-        #df = df.append(customDF)
-    i=i+1
+        # IntervalTimer = pd.Timedelta(abs(pd.Timestamp(processedData['QueryTime'][l+1]) - pd.Timestamp(processedData['QueryTime'][l]))).seconds
+        # if IntervalTimer <=60:
 
-#df.to_csv(f'dataset/final.txt', index=False,sep=' ')
+
+
+
+
+
