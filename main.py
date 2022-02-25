38d6d28a03c7f566a233a5d00f08a05d992873f3
yogeswar <yogi2198@gmail.com>
2022-02-25 12:27:34 -0500

    modularity and storing output to csv

--- main.py
+++ main.py
@@ -1,48 +1,103 @@
 import pandas as pd
 import numpy as np
+import sys
+from pandas.core.dtypes.missing import isna
 
-#the size of dataset len(dataset)=3558412
-from datetime import datetime
-Q = pd.read_csv(f'DS/user-ct-test-collection-01.txt', sep='\t', header=0)
-# print(Q.head(5))
-# if Q['QueryTime'][0]!=Q['QueryTime'][1]:
-#       print((Q['QueryTime'][2]), (Q['QueryTime'][3]))
-
-x= pd.Timedelta(abs(pd.Timestamp(Q['QueryTime'][2])- pd.Timestamp(Q['QueryTime'][3]))).seconds
-# print(str(x))
-x= 4
-y=6
-if pd.notnull(Q['ItemRank'][0]):
-      print("yes")
-
-# df = pd.DataFrame({'id': np.arange(1,6,1),
-#                    'val': list('ABCDE')})
-#print(Q['AnonID'])
-# df = pd.DataFrame(columns=['index','AnonId', 'Query', 'Query change', 'Query Date', 'Query Time'])
-i=0
-group_config = Q.groupby('AnonID')
-for k,gp in group_config:
-    # if(k == 142):
-    filterWord = gp[gp["ClickURL"].notnull()]
-    processedData = filterWord["QueryTime"].astype('datetime64[ns]').to_frame().reset_index()
-    processedData.columns = ['index', 'QueryTime']
-    l=0
-    for l in processedData.index:
-        # config_var = gp.index[l] if k == 142 else gp.index[l-1]
-        config_var = gp.index[l]
-        prev_var_time =gp.loc[config_var]['QueryTime']
-        IntervalTimer = pd.Timedelta(abs(pd.Timestamp(processedData['QueryTime'][l]) - pd.Timestamp(prev_var_time))).seconds
-        if IntervalTimer >60:
-            print('('+str(k) + ','+gp.loc[filterWord.index[l]]['Query'] +',' +'NULL, [(1,' + gp.loc[filterWord.index[l]]['ClickURL']+')])')
-            # (142, westchester.gov, NULL, [(1, http: // www.westchestergov.com)])
-        else:
-            continue
-
-        # IntervalTimer = pd.Timedelta(abs(pd.Timestamp(processedData['QueryTime'][l+1]) - pd.Timestamp(processedData['QueryTime'][l]))).seconds
-        # if IntervalTimer <=60:
+dataset_name = sys.argv[1]
+time_interval = sys.argv[2]
 
 
+def read_ds(ds):
+    data = pd.read_csv(f'DS/{ds}.txt', sep='\t')
+    return data
 
+def store_result(ds,result_list):
+    output_store = pd.DataFrame(np.asarray(result_list), columns=["AnonID", "Query", "new Query", "Generated URLs"])
+    output_store.to_csv(f'./output/{ds}-out.csv', index=False)
 
+def generate_query(ds_name, interval):
+    interval = int(interval)
+    groups = read_ds(ds_name).groupby("AnonID")
+    results = []
+    for group_id, group in groups:
+        group_reverse = group.iloc[::-1]
 
+        groupLength = group_reverse.shape[0]
+        skip = -1
+        for row in range(groupLength):
+            if row <= skip:
+                continue
+            currentRow = group_reverse.iloc[row]
+            resultURLs = []
+            userQuery = []
+            correctQuery = []
+            updatedQuery = []
+            previousRowIndex = row + 1
 
+            if previousRowIndex >= groupLength:
+                if not isna(currentRow["ClickURL"]):
+                    result = (
+                        currentRow["AnonID"], currentRow["Query"], "NULL",
+                        [(currentRow["ItemRank"], currentRow["ClickURL"])])
+                    results.append(result)
+                    print(currentRow["QueryTime"], result)
+                continue
+
+            previousRow = group_reverse.iloc[previousRowIndex]
+
+            timeDiff = pd.Timedelta(
+                abs(pd.Timestamp(currentRow["QueryTime"]) - pd.Timestamp(previousRow["QueryTime"]))).seconds
+
+            if timeDiff > interval and not isna(currentRow["ClickURL"]):
+                result = (
+                    currentRow["AnonID"], currentRow["Query"], "NULL",
+                    [(currentRow["ItemRank"], currentRow["ClickURL"])])
+                results.append(result)
+                print(currentRow["QueryTime"], result)
+            else:
+                concurrentRowIndex = 0
+                for concurrentRowIndex in range(row, groupLength):
+                    concurrentRow = group_reverse.iloc[concurrentRowIndex]
+                    timeDiff = pd.Timedelta(
+                        abs(pd.Timestamp(currentRow["QueryTime"]) - pd.Timestamp(concurrentRow["QueryTime"]))).seconds
+
+                    if interval >= timeDiff >= 0:
+                        if not isna(concurrentRow["ClickURL"]):
+                            resultURLs.append((concurrentRow["ItemRank"], concurrentRow["ClickURL"]))
+                            if concurrentRow["Query"] == currentRow["Query"]:
+                                updatedQuery.append("NULL")
+                            else:
+                                updatedQuery.append(currentRow["Query"])
+                            if concurrentRow["QueryTime"] == currentRow["QueryTime"] and concurrentRow.name != currentRow.name:
+                                correctQuery.append(concurrentRow)
+                        if isna(concurrentRow["ClickURL"]):
+                            userQuery.append(concurrentRow)
+                            updatedQuery.append(currentRow["Query"])
+                    else:
+                        break
+
+                if resultURLs:
+                    if userQuery:
+                        for i, query in enumerate(userQuery):
+                            result = (query["AnonID"], query["Query"], updatedQuery[i], resultURLs)
+                            results.append(result)
+                            print(query["QueryTime"], len(userQuery), result)
+                    elif correctQuery:
+                        for i, query in enumerate(correctQuery):
+                            result = (query["AnonID"], query["Query"], updatedQuery[i], resultURLs)
+                            results.append(result)
+                            if i > 0:
+                                if correctQuery[i]["Query"] != correctQuery[i - 1]["Query"] or correctQuery[i][
+                                    "QueryTime"] != correctQuery[i - 1]["QueryTime"]:
+                                    print(query["QueryTime"], len(userQuery), result)
+
+                            else:
+                                print(query["QueryTime"], len(userQuery), result)
+                        if concurrentRowIndex == groupLength - 1 or concurrentRowIndex == groupLength:
+                            print(
+                                f"{concurrentRow['QueryTime']} ({concurrentRow['AnonID']},'{concurrentRow['Query']}','NULL',[({concurrentRow['ItemRank']},'{concurrentRow['ClickURL']})])'")
+                            # results.append(query["AnonID"], query["Query"], updatedQuery[i], resultURLs)
+
+                skip = concurrentRowIndex
+    store_result(dataset_name,results)
+generate_query(dataset_name,time_interval)
